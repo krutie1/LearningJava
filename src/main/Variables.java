d2075c687937620780dbd1385beb2d7e9f702d7c
cloderno <cloderno@gmail.com>
2024-04-01 22:01:58 +0600

    vars

---
+++ src/main/Variables.java
@@ -0,0 +1,10 @@
+package main;
+
+public class Variables {
+    public static void main(String[] args) {
+        int number = 10;
+        String str = "string";
+
+        System.out.println();
+    }
+}
